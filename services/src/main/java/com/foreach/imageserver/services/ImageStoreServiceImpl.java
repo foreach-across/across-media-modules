fc9fc7d0f56f99b40b17e8daabf4d7a97bc290f2
Arne Vandamme <arne@foreach.be>
2013-07-09 10:30:35 +0200

    Implemented basic ImageStore

--- services/src/main/java/com/foreach/imageserver/services/ImageStoreServiceImpl.java
+++ services/src/main/java/com/foreach/imageserver/services/ImageStoreServiceImpl.java
@@ -1,11 +1,16 @@
 package com.foreach.imageserver.services;
 
 import com.foreach.imageserver.business.Image;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.FastDateFormat;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.InputStream;
 
 @Service
@@ -14,13 +19,13 @@ public class ImageStoreServiceImpl implements ImageStoreService
 	private static final Logger LOG = Logger.getLogger( ImageStoreServiceImpl.class );
 	private static final FastDateFormat PATH_FORMAT = FastDateFormat.getInstance( "/yyyy/MM/dd/" );
 
-	private String originalBasePath;
-	private String variantBasePath;
+	private final File originalBasePath;
+	private final File variantBasePath;
 
 	public ImageStoreServiceImpl( @Value("store.original.path") String originalBasePath,
 	                              @Value("store.variant.path") String variantBasePath ) {
-		this.originalBasePath = originalBasePath;
-		this.variantBasePath = variantBasePath;
+		this.originalBasePath = new File( originalBasePath );
+		this.variantBasePath = new File( variantBasePath );
 	}
 
 	@Override
@@ -30,11 +35,72 @@ public class ImageStoreServiceImpl implements ImageStoreService
 
 	@Override
 	public long saveImage( Image image, InputStream imageData ) {
-		return 0;  //To change body of implemented methods use File | Settings | File Templates.
+		String path = createPathForOriginal( image );
+		String fileName = createFileName( image );
+
+		try {
+			FileOutputStream fos = new FileOutputStream( path + "/" + fileName );
+
+			try {
+				return IOUtils.copy( imageData, fos );
+			}
+			finally {
+				fos.flush();
+				IOUtils.closeQuietly( fos );
+				IOUtils.closeQuietly( imageData );
+			}
+		}
+		catch ( Exception ioe ) {
+			LOG.error( "Unable to save image: ", ioe );
+			throw new ImageStoreOperationException( ioe );
+		}
 	}
 
 	@Override
 	public void deleteVariants( Image image ) {
-		//To change body of implemented methods use File | Settings | File Templates.
+		try {
+			String path = createPathForVariant( image );
+			final String fileNamePrefix = image.getId() + ".";
+
+			File[] variants = new File( path ).listFiles( new FilenameFilter()
+			{
+				@Override
+				public boolean accept( File dir, String name ) {
+					return StringUtils.startsWithIgnoreCase( name, fileNamePrefix );
+				}
+			} );
+
+			for ( File variant : variants ) {
+				variant.delete();
+			}
+		}
+		catch ( Exception e ) {
+			LOG.warn( "Failed to delete variants for image ", e );
+			throw new ImageStoreOperationException( e );
+		}
+	}
+
+	private String createFileName( Image image ) {
+		return image.getId() + "." + image.getImageType().getExtension();
+	}
+
+	private String createPathForOriginal( Image image ) {
+		return createPathForImage( originalBasePath, image );
+	}
+
+	private String createPathForVariant( Image image ) {
+		return createPathForImage( variantBasePath, image );
+	}
+
+	private String createPathForImage( File basePath, Image image ) {
+		File path = new File( basePath.getAbsolutePath() + "/" + image.getApplicationId() +
+				                      "/" + image.getFilePath() );
+		if ( !path.exists() ) {
+			if ( path.mkdirs() ) {
+				LOG.debug( "Created new file location " + path.getAbsolutePath() );
+			}
+		}
+
+		return path.getAbsolutePath();
 	}
 }
--- services/src/test/java/com/foreach/imageserver/services/TestImageStoreService.java
+++ services/src/test/java/com/foreach/imageserver/services/TestImageStoreService.java
@@ -1,10 +1,18 @@
 package com.foreach.imageserver.services;
 
 import com.foreach.imageserver.business.Image;
+import com.foreach.imageserver.business.ImageType;
 import com.foreach.shared.utils.DateUtils;
 import com.foreach.test.MockedLoader;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,15 +20,43 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
+import java.io.*;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @ContextConfiguration(classes = { TestImageStoreService.TestConfig.class }, loader = MockedLoader.class)
 public class TestImageStoreService
 {
+	private static final Logger LOG = LoggerFactory.getLogger( TestImageStoreService.class );
+
+	private static String TEMP_DIR;
+	private static String ORIGINAL_STORE;
+	private static String VARIANT_STORE;
+
 	@Autowired
 	private ImageStoreService imageStoreService;
+
+	@BeforeClass
+	public static void createStorePaths() {
+		TEMP_DIR =
+				System.getProperty( "java.io.tmpdir" ) + "/imgsrv-" + RandomStringUtils.randomAlphanumeric( 15 ) + "/";
+
+		assertFalse( "Temp image store directory should not yet exist", new File( TEMP_DIR ).exists() );
+		ORIGINAL_STORE = TEMP_DIR + "original";
+		VARIANT_STORE = TEMP_DIR + "/variant";
+	}
+
+	@AfterClass
+	public static void cleanupStorePaths() {
+		try {
+			FileUtils.deleteDirectory( new File( TEMP_DIR ) );
+		}
+		catch ( IOException ioe ) {
+			LOG.warn( "Could not cleanup unit test path: ", ioe );
+		}
+	}
 
 	@Test
 	public void generateRelativeImagePath() {
@@ -32,12 +68,101 @@ public class TestImageStoreService
 		assertEquals( "/2013/07/06/", path );
 	}
 
+	@Test
+	public void saveNewImage() throws Exception {
+		Image image = new Image();
+		image.setId( 2 );
+		image.setApplicationId( 10 );
+		image.setFilePath( "/2013/07/09/" );
+		image.setImageType( ImageType.JPEG );
+
+		saveAndVerify( image, ImageTestData.EARTH, ORIGINAL_STORE, "/10/2013/07/09/2.jpeg" );
+	}
+
+	@Test
+	public void updateImage() throws Throwable {
+		Image image = new Image();
+		image.setId( 3 );
+		image.setApplicationId( 10 );
+		image.setFilePath( "/2013/07/09/" );
+		image.setImageType( ImageType.JPEG );
+
+		saveAndVerify( image, ImageTestData.EARTH, ORIGINAL_STORE, "/10/2013/07/09/3.jpeg" );
+		saveAndVerify( image, ImageTestData.SUNSET, ORIGINAL_STORE, "/10/2013/07/09/3.jpeg" );
+	}
+
+	private void saveAndVerify( Image image,
+	                            ImageTestData testData,
+	                            String path,
+	                            String expectedFileName ) throws Exception {
+		InputStream imageData = getClass().getResourceAsStream( testData.getResourcePath() );
+
+		File expectedFile = new File( path, expectedFileName );
+		long fileSize = imageStoreService.saveImage( image, imageData );
+
+		assertEquals( testData.getFileSize(), fileSize );
+		assertTrue( expectedFile.exists() );
+		FileInputStream fos = new FileInputStream( expectedFile );
+		assertTrue( IOUtils.contentEquals( getClass().getResourceAsStream( testData.getResourcePath() ), fos ) );
+		fos.close();
+	}
+
+	@Test(expected = ImageStoreOperationException.class)
+	public void failSavingOriginal() throws Exception {
+		Image image = new Image();
+		image.setId( 2 );
+		image.setApplicationId( 10 );
+		image.setFilePath( "/2013/07/09/" );
+		image.setImageType( ImageType.JPEG );
+
+		imageStoreService.saveImage( image, null );
+	}
+
+	@Test
+	public void deleteVariants() throws Exception {
+		Image image = new Image();
+		image.setId( 2 );
+		image.setApplicationId( 10 );
+		image.setFilePath( "/2013/07/06/" );
+		image.setImageType( ImageType.JPEG );
+
+		// Put dummy files in place
+		new File( VARIANT_STORE, "/10/2013/07/06/" ).mkdirs();
+		new File( ORIGINAL_STORE, "/10/2013/07/06/" ).mkdirs();
+
+		File original = createDummy( ORIGINAL_STORE, "/10/2013/07/06/2.jpeg" );
+		File variantOne = createDummy( VARIANT_STORE, "/10/2013/07/06/2.100x200.jpeg" );
+		File variantTwo = createDummy( VARIANT_STORE, "/10/2013/07/06/2.test.png" );
+		File otherVariant = createDummy( VARIANT_STORE, "/10/2013/07/06/3.100x200.jpeg" );
+
+		assertTrue( original.exists() );
+		assertTrue( variantOne.exists() );
+		assertTrue( variantTwo.exists() );
+		assertTrue( otherVariant.exists() );
+
+		imageStoreService.deleteVariants( image );
+
+		assertTrue( original.exists() );
+		assertFalse( variantOne.exists() );
+		assertFalse( variantTwo.exists() );
+		assertTrue( otherVariant.exists() );
+	}
+
+	private File createDummy( String path, String fileName ) throws Exception {
+		File file = new File( path, fileName );
+		FileOutputStream fos = new FileOutputStream( file );
+		IOUtils.write( new byte[0], fos );
+		fos.close();
+
+		return file;
+	}
+
 	@Configuration
 	public static class TestConfig
 	{
 		@Bean
 		public ImageStoreService imageStoreService() {
-			return new ImageStoreServiceImpl( "", "" );
+			return new ImageStoreServiceImpl( ORIGINAL_STORE, VARIANT_STORE );
 		}
 	}
 }
