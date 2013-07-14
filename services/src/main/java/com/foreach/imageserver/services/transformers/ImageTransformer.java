3a6700af133cc50ec012234f93961f29192e4cc4
Arne Vandamme <arne@foreach.be>
2013-07-14 10:59:56 +0200

    Javadoc on ImageTransformer basics

--- services/src/main/java/com/foreach/imageserver/services/transformers/ImageTransformer.java
+++ services/src/main/java/com/foreach/imageserver/services/transformers/ImageTransformer.java
@@ -5,9 +5,32 @@ import com.foreach.imageserver.business.ImageModifier;
 
 public interface ImageTransformer
 {
+	/**
+	 * Checks if the instance can execute a certain modification (without actually doing it)
+	 * and returns the corresponding priority enum value.
+	 *
+	 * @param original Original file to be modified.
+	 * @param modifier Modification to be done.
+	 * @return ImageTransformerPriority value
+	 */
 	ImageTransformerPriority canApply( ImageFile original, ImageModifier modifier );
 
+	/**
+	 * Applies the actual modification on an image.  If the canApply() method is implemented
+	 * correctly, the actual apply() can rely on only being called if the canApply() did not return UNABLE.
+	 *
+	 * @param original Original file to be modified.
+	 * @param modifier Modification to be done.
+	 * @return ImageFile with the modified image
+	 */
 	ImageFile apply( ImageFile original, ImageModifier modifier );
 
+	/**
+	 * Determines the preferred order into which this transformer is applied, if it can
+	 * handle the modification and others can as well.
+	 * Transformers with higher priority are called first.
+	 *
+	 * @return Priority as int.
+	 */
 	int getPriority();
 }
--- services/src/main/java/com/foreach/imageserver/services/transformers/ImageTransformerPriority.java
+++ services/src/main/java/com/foreach/imageserver/services/transformers/ImageTransformerPriority.java
@@ -1,5 +1,13 @@
 package com.foreach.imageserver.services.transformers;
 
+/**
+ * <p>Represents the ImageTransformer feedback on a request for modification:
+ * <ul>
+ *     <li><strong>UNABLE</strong>: Not able to handle the modification</li>
+ *     <li><strong>PREFERRED</strong>: Can and want to do this modification</li>
+ *     <li><strong>FALLBACK</strong>: Can do this modification but only try if no preferred transformers available</li>
+ * </ul></p>
+ */
 public enum ImageTransformerPriority
 {
 	UNABLE,
