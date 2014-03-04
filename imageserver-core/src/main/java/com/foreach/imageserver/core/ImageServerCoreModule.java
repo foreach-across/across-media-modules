2ea3f83dcb97042b762dfb215317be862387b231
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-03-04 11:35:40 +0100

    Only expose @Exposed beans.

--- imageserver-core/src/main/java/com/foreach/imageserver/core/ImageServerCoreModule.java
+++ imageserver-core/src/main/java/com/foreach/imageserver/core/ImageServerCoreModule.java
@@ -1,12 +1,18 @@
 package com.foreach.imageserver.core;
 
 import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.annotations.Exposed;
 import com.foreach.across.core.context.configurer.AnnotatedClassConfigurer;
 import com.foreach.across.core.context.configurer.ApplicationContextConfigurer;
+import com.foreach.across.core.filters.AnnotationBeanFilter;
 
 import java.util.Set;
 
 public class ImageServerCoreModule extends AcrossModule {
+
+    public ImageServerCoreModule() {
+        setExposeFilter(new AnnotationBeanFilter( true, Exposed.class ));
+    }
 
     @Override
     public String getName() {
