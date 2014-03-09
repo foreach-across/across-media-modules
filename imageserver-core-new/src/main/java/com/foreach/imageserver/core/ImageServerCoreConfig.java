a61df102b58115a463d30c058e9cc9a9cc73c2e0
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-03-09 10:23:57 +0100

    Introduced the imageserver-core-new module.

---
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/ImageServerCoreConfig.java
@@ -0,0 +1,54 @@
+package com.foreach.imageserver.core;
+
+import com.foreach.across.core.annotations.Exposed;
+import liquibase.integration.spring.SpringLiquibase;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.foreach.imageserver.core", excludeFilters = @ComponentScan.Filter(Configuration.class))
+@MapperScan("com.foreach.imageserver.core.data")
+public class ImageServerCoreConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    @Exposed
+    public RequestMappingHandlerMapping imageServerCoreHandlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors(new Object[]{});
+        return handlerMapping;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public org.apache.ibatis.session.SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeAliases(new Class[]{});
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SpringLiquibase springLiquibase(DataSource dataSource) {
+        // TODO Should become an Across installer.
+        SpringLiquibase springLiquibase = new SpringLiquibase();
+        springLiquibase.setDataSource(dataSource);
+        springLiquibase.setChangeLog("classpath:com/foreach/imageserver/core/liquibase/changelog.xml");
+        return springLiquibase;
+    }
+
+}
---
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/ImageServerCoreModule.java
@@ -0,0 +1,32 @@
+package com.foreach.imageserver.core;
+
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.annotations.Exposed;
+import com.foreach.across.core.context.configurer.AnnotatedClassConfigurer;
+import com.foreach.across.core.context.configurer.ApplicationContextConfigurer;
+import com.foreach.across.core.filters.AnnotationBeanFilter;
+
+import java.util.Set;
+
+public class ImageServerCoreModule extends AcrossModule {
+
+    public ImageServerCoreModule() {
+        setExposeFilter(new AnnotationBeanFilter(true, Exposed.class));
+    }
+
+    @Override
+    public String getName() {
+        return "ImageServerCore";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Foreach Image Server Core Module";
+    }
+
+    @Override
+    protected void registerDefaultApplicationContextConfigurers(Set<ApplicationContextConfigurer> contextConfigurers) {
+        contextConfigurers.add(new AnnotatedClassConfigurer(ImageServerCoreConfig.class));
+    }
+
+}
