64d74700d90983ef55f7e0a15530fb089aa6aff9
Koen Deschacht <kds@foreach.be>
2014-03-10 12:46:18 +0100

    Removed ImageDeleteController.java, no height and width in ImageModificationDto.java

--- imageserver-core-new/src/main/java/com/foreach/imageserver/core/web/dto/ImageModificationDto.java
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/web/dto/ImageModificationDto.java
@@ -7,7 +7,6 @@ public class ImageModificationDto {
 
     private Crop crop = new Crop();
     private Dimensions density = new Dimensions();
-    private Integer width, height;
     private boolean stretch = false;
     private boolean keepAspect = true;
 
@@ -25,22 +24,6 @@ public class ImageModificationDto {
 
     public void setDensity(Dimensions density) {
         this.density = density;
-    }
-
-    public Integer getWidth() {
-        return width;
-    }
-
-    public void setWidth(Integer width) {
-        this.width = width;
-    }
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
     }
 
     public boolean isStretch() {
