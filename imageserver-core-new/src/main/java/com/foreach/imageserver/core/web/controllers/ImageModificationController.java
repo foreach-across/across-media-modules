59117637d399a734d658c1aee9b8a2632a7e7504
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-03-14 19:11:01 +0100

    First steps for the ImageLoadController.

--- imageserver-core-new/src/main/java/com/foreach/imageserver/core/web/controllers/ImageModificationController.java
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/web/controllers/ImageModificationController.java
@@ -1,9 +1,16 @@
 package com.foreach.imageserver.core.web.controllers;
 
+import com.foreach.imageserver.core.business.Application;
+import com.foreach.imageserver.core.business.Image;
+import com.foreach.imageserver.core.business.ImageModification;
+import com.foreach.imageserver.core.business.ImageResolution;
+import com.foreach.imageserver.core.services.ApplicationService;
+import com.foreach.imageserver.core.services.ImageService;
 import com.foreach.imageserver.core.web.displayables.JsonResponse;
 import com.foreach.imageserver.core.web.dto.ImageModificationDto;
 import com.foreach.imageserver.core.web.dto.ImageResolutionDto;
 import com.foreach.imageserver.core.web.dto.RegisteredImageModificationDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,14 +27,43 @@ public class ImageModificationController extends BaseImageAPIController {
     public static final String LIST_REGISTERED_PATH = "listRegistered";
     public static final String LIST_RESOLUTIONS_PATH = "listResolutions";
 
-    @RequestMapping(value = "/" + REGISTER_PATH, method = RequestMethod.POST)
+    @Autowired
+    private ApplicationService applicationService;
+
+    @Autowired
+    private ImageService imageService;
+
+    @RequestMapping(value = "/" + REGISTER_PATH, method = { RequestMethod.GET, RequestMethod.POST })
     @ResponseBody
     public JsonResponse register(@RequestParam(value = "aid", required = true) int applicationId,
                                  @RequestParam(value = "token", required = true) String applicationKey,
                                  @RequestParam(value = "iid", required = true) int imageId,
                                  ImageResolutionDto imageResolutionDto,
                                  ImageModificationDto imageModificationDto) {
-        //TODO
+        Application application = applicationService.getById(applicationId);
+        if (application == null || !application.canBeManaged(applicationKey)) {
+            return error(String.format("Unknown application %s.", applicationId));
+        }
+
+        Image image = imageService.getById(applicationId, imageId);
+        if (image == null) {
+            return error(String.format("No image exists for application id %d and image id %d.", applicationId, imageId));
+        }
+
+        ImageResolution imageResolution = applicationService.getImageResolution(applicationId, imageResolutionDto.getWidth(), imageResolutionDto.getHeight());
+        if (imageResolution == null) {
+            return error("Supplied image resolution is not valid for this application.");
+        }
+
+        ImageModification modification = new ImageModification();
+        modification.setApplicationId(applicationId);
+        modification.setImageId(imageId);
+        modification.setResolutionId(imageResolution.getId());
+        modification.setCrop(imageModificationDto.getCrop());
+        modification.setDensity(imageModificationDto.getDensity());
+
+        imageService.saveImageModification(modification);
+
         return success();
     }
 
--- imageserver-core-new/src/main/java/com/foreach/imageserver/core/web/dto/ImageModificationDto.java
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/web/dto/ImageModificationDto.java
@@ -7,8 +7,6 @@ public class ImageModificationDto {
 
     private Crop crop = new Crop();
     private Dimensions density = new Dimensions();
-    private boolean stretch = false;
-    private boolean keepAspect = true;
 
     public Crop getCrop() {
         return crop;
@@ -24,21 +22,5 @@ public class ImageModificationDto {
 
     public void setDensity(Dimensions density) {
         this.density = density;
-    }
-
-    public boolean isStretch() {
-        return stretch;
-    }
-
-    public void setStretch(boolean stretch) {
-        this.stretch = stretch;
-    }
-
-    public boolean isKeepAspect() {
-        return keepAspect;
-    }
-
-    public void setKeepAspect(boolean keepAspect) {
-        this.keepAspect = keepAspect;
     }
 }
