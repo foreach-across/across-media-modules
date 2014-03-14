cbff14e64925d904e2b04fe86ae9a90f759afe4c
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-03-14 19:02:40 +0100

    Introduced ApplicationService::getImageResolution.

--- imageserver-core-new/src/main/java/com/foreach/imageserver/core/services/ApplicationService.java
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/services/ApplicationService.java
@@ -1,7 +1,10 @@
 package com.foreach.imageserver.core.services;
 
 import com.foreach.imageserver.core.business.Application;
+import com.foreach.imageserver.core.business.ImageResolution;
 
 public interface ApplicationService {
     Application getById(int id);
+
+    ImageResolution getImageResolution(int applicationId, Integer width, Integer height);
 }
--- imageserver-core-new/src/main/java/com/foreach/imageserver/core/services/ApplicationServiceImpl.java
+++ imageserver-core-new/src/main/java/com/foreach/imageserver/core/services/ApplicationServiceImpl.java
@@ -1,9 +1,13 @@
 package com.foreach.imageserver.core.services;
 
 import com.foreach.imageserver.core.business.Application;
+import com.foreach.imageserver.core.business.ImageResolution;
 import com.foreach.imageserver.core.data.ApplicationDao;
+import com.foreach.imageserver.core.data.ImageResolutionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ApplicationServiceImpl implements ApplicationService {
@@ -11,9 +15,25 @@ public class ApplicationServiceImpl implements ApplicationService {
     @Autowired
     private ApplicationDao applicationDao;
 
+    @Autowired
+    private ImageResolutionDao imageResolutionDao;
+
     @Override
     public Application getById(int id) {
         return applicationDao.getById(id);
+    }
+
+    // TODO Move logic directly into the data layer please; let's not iterate.
+    @Override
+    public ImageResolution getImageResolution(int applicationId, Integer width, Integer height) {
+        List<ImageResolution> imageResolutions = imageResolutionDao.getForApplication(applicationId);
+        for (ImageResolution imageResolution : imageResolutions) {
+            if (imageResolution.getWidth() == width && imageResolution.getHeight() == height) {
+                return imageResolution;
+            }
+        }
+
+        return null;
     }
 
 }
