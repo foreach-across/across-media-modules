de9d89482038c80a0d218bfd7efe0eb083f6c7ec
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-03-19 16:13:23 +0100

    Configured database access for the diocontent module. Introduced the DIO_CONTENT_IMAGE_PARAMETERS schema.

--- imageserver-diocontent/src/main/java/be/mediafin/imageserver/imagerepositories/diocontent/ImageServerDioContentConfig.java
+++ imageserver-diocontent/src/main/java/be/mediafin/imageserver/imagerepositories/diocontent/ImageServerDioContentConfig.java
@@ -1,7 +1,39 @@
 package be.mediafin.imageserver.imagerepositories.diocontent;
 
+import be.mediafin.imageserver.imagerepositories.diocontent.business.DioContentImageParameters;
+import liquibase.integration.spring.SpringLiquibase;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
+@ComponentScan(basePackages = "be.mediafin.imageserver.imagerepositories.diocontent", excludeFilters = @ComponentScan.Filter(Configuration.class))
+@MapperScan("be.mediafin.imageserver.imagerepositories.diocontent.data")
 public class ImageServerDioContentConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public org.apache.ibatis.session.SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setTypeAliases(new Class[]{DioContentImageParameters.class});
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SpringLiquibase springLiquibase(DataSource dataSource) {
+        // TODO Should become an Across installer.
+        SpringLiquibase springLiquibase = new SpringLiquibase();
+        springLiquibase.setDataSource(dataSource);
+        springLiquibase.setChangeLog("classpath:be/mediafin/imageserver/imagerepositories/diocontent/liquibase/changelog.xml");
+        return springLiquibase;
+    }
+
 }
