832d7e07caf165c8ffd656b8a4fb3d53201fe9b6
Alain Van Hout <avh@foreach.be>
2014-03-28 15:52:38 +0100

    PROJ-3318: removed image resolutions with width=0, which fall outside what is currently in use and will require specific behaviour if they are ever re-added

--- web.imageserver.front/src/main/java/be/mediafin/imageserver/front/mfn/installers/MfnImageResolutionsInstaller.java
+++ web.imageserver.front/src/main/java/be/mediafin/imageserver/front/mfn/installers/MfnImageResolutionsInstaller.java
@@ -3,6 +3,6 @@ package be.mediafin.imageserver.front.mfn.installers;
 import com.foreach.across.core.annotations.Installer;
 import com.foreach.across.core.installers.AcrossLiquibaseInstaller;
 
-@Installer(description = "Inserts MFN image resolutions in DB", version = 2)
+@Installer(description = "Inserts MFN image resolutions in DB", version = 3)
 public class MfnImageResolutionsInstaller extends AcrossLiquibaseInstaller {
 }
