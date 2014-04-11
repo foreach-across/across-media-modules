352b8253cce8bd87daf13f51d18da5a2cbff7ae2
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-04-11 15:44:28 +0200

    Introduced the imageserver-imagerepository-web module.

---
+++ imageserver-imagerepository-web/src/main/java/com/foreach/imageserver/imagerepositories/web/WebImageRepositoryConfig.java
@@ -0,0 +1,7 @@
+package com.foreach.imageserver.imagerepositories.web;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebImageRepositoryConfig {
+}
---
+++ imageserver-imagerepository-web/src/main/java/com/foreach/imageserver/imagerepositories/web/WebImageRepositoryModule.java
@@ -0,0 +1,33 @@
+package com.foreach.imageserver.imagerepositories.web;
+
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.annotations.AcrossDepends;
+import com.foreach.across.core.annotations.Exposed;
+import com.foreach.across.core.context.configurer.AnnotatedClassConfigurer;
+import com.foreach.across.core.context.configurer.ApplicationContextConfigurer;
+import com.foreach.across.core.filters.AnnotationBeanFilter;
+
+import java.util.Set;
+
+@AcrossDepends(required = {"ImageServerCoreModule"})
+public class WebImageRepositoryModule extends AcrossModule {
+
+    public WebImageRepositoryModule() {
+        setExposeFilter(new AnnotationBeanFilter(true, Exposed.class));
+    }
+
+    @Override
+    public String getName() {
+        return "ImageServerWebImageRepository";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Mediafin Image Server Web Image Repository Module";
+    }
+
+    @Override
+    protected void registerDefaultApplicationContextConfigurers(Set<ApplicationContextConfigurer> contextConfigurers) {
+        contextConfigurers.add(new AnnotatedClassConfigurer(WebImageRepositoryConfig.class));
+    }
+}
