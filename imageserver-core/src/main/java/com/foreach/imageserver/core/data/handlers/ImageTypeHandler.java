904a26d4511a921148689385eda646332d682fb8
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-04-11 13:34:59 +0200

    Removed original implementation as we seem to have corrupted it. WE STILL NEED TO verify that we retained all functionality from the original implementation.

--- imageserver-core/src/main/java/com/foreach/imageserver/core/data/handlers/ImageTypeHandler.java
+++
@@ -1,7 +0,0 @@
-package com.foreach.imageserver.core.data.handlers;
-
-import com.foreach.imageserver.core.business.ImageType;
-import com.foreach.mybatis.enums.IdBasedEnumHandler;
-
-public class ImageTypeHandler extends IdBasedEnumHandler<ImageType> {
-}
--- imageserver-dpp-connector/src/main/java/com/foreach/imageserver/connectors/dpp/ws/WebServiceInvoker.java
+++
@@ -1,5 +0,0 @@
-package com.foreach.imageserver.connectors.dpp.ws;
-
-public interface WebServiceInvoker {
-    WebServiceResult invoke(WebServiceRequest request);
-}
