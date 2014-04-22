5144d091d3da00940abac97c280c3364e24d030e
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-04-22 12:43:22 +0200

    When multiple requests require generation of a variant, only one thread should actually generate the image and the others should simply re-use the same result.

--- imageserver-core/src/main/java/com/foreach/imageserver/core/services/ImageServiceImpl.java
+++ imageserver-core/src/main/java/com/foreach/imageserver/core/services/ImageServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -39,6 +40,9 @@ public class ImageServiceImpl implements ImageService {
     public Image getById(int imageId) {
         return imageDao.getById(imageId);
     }
+
+    // Used concurrently from multiple threads.
+    private Map<VariantImageRequest, FutureVariantImage> futureVariantImages = new HashMap<>();
 
     // TODO I'm not taking care of errors right now, make sure to tackle this later on!
     @Override
@@ -82,58 +86,106 @@ public class ImageServiceImpl implements ImageService {
     public StreamImageSource getVariantImage(Image image, Context context, ImageResolution imageResolution, ImageVariant imageVariant) {
         StreamImageSource imageSource = imageStoreService.getVariantImage(image, context, imageResolution, imageVariant);
         if (imageSource == null) {
-            ImageModification imageModification = imageModificationDao.getById(image.getImageId(), context.getId(), imageResolution.getId());
-            if (imageModification == null) {
-                throw new ImageCouldNotBeRetrievedException("No image modification was registered for this image.");
+            imageSource = generateVariantImage(image, context, imageResolution, imageVariant);
+        }
+        return imageSource;
+    }
+
+    /**
+     * We allow just one thread to generate a specific variant. Other threads that require this variant simultaneously
+     * will block and re-use the same result.
+     */
+    private StreamImageSource generateVariantImage(Image image, Context context, ImageResolution imageResolution, ImageVariant imageVariant) {
+        VariantImageRequest request = new VariantImageRequest(image.getImageId(), context.getId(), imageResolution.getId(), imageVariant.getOutputType());
+
+        FutureVariantImage futureVariantImage;
+        boolean otherThreadIsCreatingVariant;
+        synchronized (this) {
+            otherThreadIsCreatingVariant = futureVariantImages.containsKey(request);
+            if (otherThreadIsCreatingVariant) {
+                futureVariantImage = futureVariantImages.get(request);
+            } else {
+                futureVariantImage = new FutureVariantImage();
+                futureVariantImages.put(request, futureVariantImage);
             }
-
-            StreamImageSource originalImageSource = imageStoreService.getOriginalImage(image);
-            if (originalImageSource == null) {
-                ImageRepository imageRepository = imageRepositoryService.determineImageRepository(image.getRepositoryCode());
-                if (imageRepository == null) {
-                    throw new ImageCouldNotBeRetrievedException("Missing image repository.");
-                }
-
-                byte[] imageBytes = imageRepository.retrieveImage(image.getImageId());
-                imageStoreService.storeOriginalImage(image, imageBytes);
-                originalImageSource = new StreamImageSource(image.getImageType(), imageBytes);
-            }
-
-            Dimensions outputResolution = computeOutputResolution(image, imageResolution);
-
-            InMemoryImageSource variantImageSource = imageTransformService.modify(
-                    originalImageSource,
-                    outputResolution.getWidth(),
-                    outputResolution.getHeight(),
-                    imageModification.getCrop().getX(),
-                    imageModification.getCrop().getY(),
-                    imageModification.getCrop().getWidth(),
-                    imageModification.getCrop().getHeight(),
-                    imageModification.getDensity().getWidth(),
-                    imageModification.getDensity().getHeight(),
-                    imageVariant.getOutputType());
-
-            // TODO We might opt to catch exceptions here and not fail on the write. We can return the variant in memory regardless.
-            imageStoreService.storeVariantImage(image, context, imageResolution, imageVariant, variantImageSource.byteStream());
-
-            /**
-             * The ImageModification objects we used to determine the Crop may have changed while we were busy
-             * generating it. On the other hand, we expect the chances of this actually happening to be pretty low. To
-             * avoid having to keep database locking in mind whenever we work with ImageModification-s, we employ some
-             * semi-optimistic concurrency control. Specifically: we always write the file without any advance
-             * checking. This may cause us to serve stale variants for a very short while. Then we check that the
-             * ImageModification was not altered behind our back. Should this be the case we delete the variant from
-             * disk; it will then be recreated during the next request.
-             */
-            ImageModification reviewImageModification = imageModificationDao.getById(image.getImageId(), context.getId(), imageResolution.getId());
-            if (!imageModification.equals(reviewImageModification)) {
-                imageStoreService.removeVariantImage(image, context, imageResolution, imageVariant);
-            }
-
-            return variantImageSource.stream();
         }
 
-        return imageSource;
+        if (otherThreadIsCreatingVariant) {
+            return futureVariantImage.get();
+        } else {
+            try {
+                InMemoryImageSource variantImageSource = generateVariantImageInCurrentThread(image, context, imageResolution, imageVariant);
+                synchronized (this) {
+                    futureVariantImage.setResult(variantImageSource);
+                    futureVariantImages.remove(request);
+                }
+                return variantImageSource.stream();
+            } catch (RuntimeException e) {
+                synchronized (this) {
+                    futureVariantImage.setRuntimeException(e);
+                    futureVariantImages.remove(request);
+                }
+                throw e;
+            } catch (Error e) {
+                synchronized (this) {
+                    futureVariantImage.setError(e);
+                    futureVariantImages.remove(request);
+                }
+                throw e;
+            }
+        }
+    }
+
+    private InMemoryImageSource generateVariantImageInCurrentThread(Image image, Context context, ImageResolution imageResolution, ImageVariant imageVariant) {
+        ImageModification imageModification = imageModificationDao.getById(image.getImageId(), context.getId(), imageResolution.getId());
+        if (imageModification == null) {
+            throw new ImageCouldNotBeRetrievedException("No image modification was registered for this image.");
+        }
+
+        StreamImageSource originalImageSource = imageStoreService.getOriginalImage(image);
+        if (originalImageSource == null) {
+            ImageRepository imageRepository = imageRepositoryService.determineImageRepository(image.getRepositoryCode());
+            if (imageRepository == null) {
+                throw new ImageCouldNotBeRetrievedException("Missing image repository.");
+            }
+
+            byte[] imageBytes = imageRepository.retrieveImage(image.getImageId());
+            imageStoreService.storeOriginalImage(image, imageBytes);
+            originalImageSource = new StreamImageSource(image.getImageType(), imageBytes);
+        }
+
+        Dimensions outputResolution = computeOutputResolution(image, imageResolution);
+
+        InMemoryImageSource variantImageSource = imageTransformService.modify(
+                originalImageSource,
+                outputResolution.getWidth(),
+                outputResolution.getHeight(),
+                imageModification.getCrop().getX(),
+                imageModification.getCrop().getY(),
+                imageModification.getCrop().getWidth(),
+                imageModification.getCrop().getHeight(),
+                imageModification.getDensity().getWidth(),
+                imageModification.getDensity().getHeight(),
+                imageVariant.getOutputType());
+
+        // TODO We might opt to catch exceptions here and not fail on the write. We can return the variant in memory regardless.
+        imageStoreService.storeVariantImage(image, context, imageResolution, imageVariant, variantImageSource.byteStream());
+
+        /**
+         * The ImageModification objects we used to determine the Crop may have changed while we were busy
+         * generating it. On the other hand, we expect the chances of this actually happening to be pretty low. To
+         * avoid having to keep database locking in mind whenever we work with ImageModification-s, we employ some
+         * semi-optimistic concurrency control. Specifically: we always write the file without any advance
+         * checking. This may cause us to serve stale variants for a very short while. Then we check that the
+         * ImageModification was not altered behind our back. Should this be the case we delete the variant from
+         * disk; it will then be recreated during the next request.
+         */
+        ImageModification reviewImageModification = imageModificationDao.getById(image.getImageId(), context.getId(), imageResolution.getId());
+        if (!imageModification.equals(reviewImageModification)) {
+            imageStoreService.removeVariantImage(image, context, imageResolution, imageVariant);
+        }
+
+        return variantImageSource;
     }
 
     @Override
@@ -176,4 +228,86 @@ public class ImageServiceImpl implements ImageService {
         return dimensions;
     }
 
+    private static class VariantImageRequest {
+        private final int imageId;
+        private final int contextId;
+        private final int resolutionId;
+        private final ImageType outputType;
+
+        public VariantImageRequest(int imageId, int contextId, int resolutionId, ImageType outputType) {
+            this.imageId = imageId;
+            this.contextId = contextId;
+            this.resolutionId = resolutionId;
+            this.outputType = outputType;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            VariantImageRequest that = (VariantImageRequest) o;
+
+            if (contextId != that.contextId) return false;
+            if (imageId != that.imageId) return false;
+            if (resolutionId != that.resolutionId) return false;
+            if (outputType != that.outputType) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = imageId;
+            result = 31 * result + contextId;
+            result = 31 * result + resolutionId;
+            result = 31 * result + (outputType != null ? outputType.hashCode() : 0);
+            return result;
+        }
+    }
+
+    private static class FutureVariantImage {
+        private InMemoryImageSource result;
+        private RuntimeException exception;
+        private Error error;
+
+        public synchronized StreamImageSource get() {
+            try {
+                while (result == null && exception == null && error == null) {
+                    wait();
+                }
+
+                if (exception != null) {
+                    throw exception;
+                }
+
+                if (error != null) {
+                    throw error;
+                }
+
+                return result.stream();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new VariantGenerationWasInterruptedException();
+            }
+        }
+
+        public synchronized void setResult(InMemoryImageSource result) {
+            this.result = result;
+            notifyAll();
+        }
+
+        public synchronized void setRuntimeException(RuntimeException e) {
+            this.exception = e;
+            notifyAll();
+        }
+
+        public synchronized void setError(Error e) {
+            this.error = e;
+            notifyAll();
+        }
+    }
+
+    private static class VariantGenerationWasInterruptedException extends RuntimeException {
+    }
 }
