1b7b0e83679dc7cd49f609b04141b0be83f6d5b9
Olivier Verhoogen <Olivier.Verhoogen@foreach.be>
2014-05-09 13:20:52 +0200

    Provided an API method to check whether an Image with a certain id exists.

--- imageserver-core/src/main/java/com/foreach/imageserver/core/web/controllers/ImageLoadController.java
+++ imageserver-core/src/main/java/com/foreach/imageserver/core/web/controllers/ImageLoadController.java
@@ -1,6 +1,7 @@
 package com.foreach.imageserver.core.web.controllers;
 
 import com.foreach.imageserver.core.business.Dimensions;
+import com.foreach.imageserver.core.business.Image;
 import com.foreach.imageserver.core.services.ImageService;
 import com.foreach.imageserver.dto.DimensionsDto;
 import com.foreach.imageserver.dto.JsonResponse;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 public class ImageLoadController extends BaseImageAPIController {
 
     public static final String LOAD_IMAGE_PATH = "load";
+    public static final String IMAGE_EXISTS_PATH = "imageExists";
 
     @Value("${accessToken}")
     private String accessToken;
@@ -39,6 +41,19 @@ public class ImageLoadController extends BaseImageAPIController {
         Dimensions imageDimensions = imageService.saveImage(externalId, imageData);
 
         return success(dto(imageDimensions));
+    }
+
+    @RequestMapping(value = "/" + IMAGE_EXISTS_PATH, method = RequestMethod.GET)
+    @ResponseBody
+    public JsonResponse exists(@RequestParam(value = "token", required = true) String accessToken,
+                               @RequestParam(value = "iid", required = true) String externalId) {
+        if (!this.accessToken.equals(accessToken)) {
+            return error("Access denied.");
+        }
+
+        Image image = imageService.getByExternalId(externalId);
+
+        return success(Boolean.valueOf(image != null));
     }
 
     private DimensionsDto dto(Dimensions dimensions) {
