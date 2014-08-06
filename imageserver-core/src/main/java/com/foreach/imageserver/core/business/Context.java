8281e92e387bcb8312d93c2b472850e51d578b3d
Arne Vandamme <arne@foreach.be>
2014-08-06 11:17:53 +0200

    Rename context sequence

--- imageserver-core/src/main/java/com/foreach/imageserver/core/business/Context.java
+++ imageserver-core/src/main/java/com/foreach/imageserver/core/business/Context.java
@@ -8,43 +8,43 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name= ImageSchemaConfiguration.TABLE_CONTEXT)
-public class Context {
+@Table(name = ImageSchemaConfiguration.TABLE_CONTEXT)
+public class Context
+{
+	@Id
+	@GeneratedValue(generator = "seq_img_context_id")
+	@GenericGenerator(
+			name = "seq_img_context_id",
+			strategy = AcrossSequenceGenerator.STRATEGY,
+			parameters = {
+					@org.hibernate.annotations.Parameter(name = "sequenceName", value = "seq_img_context_id"),
+					@org.hibernate.annotations.Parameter(name = "allocationSize", value = "10")
+			}
+	)
+	private long id;
 
-    @Id
-    @GeneratedValue( generator = "seq_context_id" )
-    @GenericGenerator(
-            name = "seq_context_id",
-            strategy = AcrossSequenceGenerator.STRATEGY,
-            parameters = {
-                    @org.hibernate.annotations.Parameter( name = "sequenceName", value = "seq_context_id" ),
-                    @org.hibernate.annotations.Parameter( name = "allocationSize", value = "10" )
-            }
-    )
-    private long id;
+	/**
+	 * Careful: We use the code to generate an intelligible folder structure. Make sure that it can be used as a valid
+	 * folder name.
+	 */
+	@Column(name = "code")
+	private String code;
 
-    /**
-     * Careful: We use the code to generate an intelligible folder structure. Make sure that it can be used as a valid
-     * folder name.
-     */
-    @Column( name = "code" )
-    private String code;
+	public long getId() {
+		return id;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public void setId( long id ) {
+		this.id = id;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public String getCode() {
+		return code;
+	}
 
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
+	public void setCode( String code ) {
+		this.code = code;
+	}
 
 	@Override
 	public boolean equals( Object o ) {
