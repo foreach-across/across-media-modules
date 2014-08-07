ac49b92e24b4bac7c70f1c44c6d872f9bf2d0629
Arne Vandamme <arne@foreach.be>
2014-08-07 09:39:29 +0200

    Delete MF specific frontend project

--- web.imageserver.front/src/main/java/com/foreach/imageserver/front/mfn/installers/MfnImageResolutionsInstaller.java
+++
@@ -1,8 +0,0 @@
-package com.foreach.imageserver.front.mfn.installers;
-
-import com.foreach.across.core.annotations.Installer;
-import com.foreach.across.core.installers.AcrossLiquibaseInstaller;
-
-@Installer(description = "Inserts MFN image resolutions in DB", version = 3)
-public class MfnImageResolutionsInstaller extends AcrossLiquibaseInstaller {
-}
