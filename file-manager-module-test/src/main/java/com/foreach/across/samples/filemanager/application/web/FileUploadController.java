eff517264f86d0da194f5533df179cf8059d2a75
Steven Gentens <stg@foreach.be>
2018-08-21 22:14:39 +0200

    AXFMM-4 added apache commons codec dependency - added test application

---
+++ file-manager-module-test/src/main/java/com/foreach/across/samples/filemanager/application/web/FileUploadController.java
@@ -0,0 +1,31 @@
+package com.foreach.across.samples.filemanager.application.web;
+
+import com.foreach.across.modules.filemanager.business.file.reference.FileReference;
+import com.foreach.across.modules.filemanager.business.file.reference.FileReferenceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author Steven Gentens
+ */
+@Controller
+@RequiredArgsConstructor
+public class FileUploadController {
+
+    private final FileReferenceService fileReferenceService;
+
+    @GetMapping("/upload")
+    public String renderForm() {
+        return "th/fileManagerTest/file-upload";
+    }
+
+    @PostMapping("/uploadFile")
+    public String processFile(@RequestParam(name = "file") MultipartFile file) {
+        FileReference reference = fileReferenceService.save(file);
+        return "redirect:/upload";
+    }
+}
