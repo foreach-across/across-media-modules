52db46267d41f8344517af71d50497483db7e0a4
Marc Vanbrabant <marc@foreach.be>
2018-09-03 16:50:10 +0200

    IMGSERVER-21 Fix count @Query for Oracle and ms-sql

--- imageserver-core/src/main/java/com/foreach/imageserver/core/repositories/ImageModificationRepository.java
+++ imageserver-core/src/main/java/com/foreach/imageserver/core/repositories/ImageModificationRepository.java
@@ -21,6 +21,9 @@ public interface ImageModificationRepository extends JpaRepository<ImageModifica
 	@Query("select i from ImageModification i where i.id.imageId = :imageId")
 	List<ImageModification> getAllModifications( @Param("imageId") long imageId );
 
-	@Query("select count(i) AS num_items from ImageModification i where i.id.imageId = :imageId AND num_items>0")
-	boolean hasModification( @Param("imageId") long imageId );
+	default boolean hasModification( long imageId ) {
+		return countImageModificationsByIdImageId( imageId ) > 0;
+	}
+
+	Long countImageModificationsByIdImageId( @Param("imageId") long imageId );
 }
