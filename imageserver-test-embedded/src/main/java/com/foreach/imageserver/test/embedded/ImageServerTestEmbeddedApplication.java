b54d0c80c8ea8f3f37e4f62bb7a67d3ca9336a06
Steven Gentens <stg@foreach.be>
2018-12-11 10:20:42 +0100

    Clean up embedded application (pom/ application descriptor)

--- imageserver-test-embedded/src/main/java/com/foreach/imageserver/test/embedded/ImageServerTestEmbeddedApplication.java
+++ imageserver-test-embedded/src/main/java/com/foreach/imageserver/test/embedded/ImageServerTestEmbeddedApplication.java
@@ -2,15 +2,10 @@ package com.foreach.imageserver.test.embedded;
 
 import com.foreach.across.config.AcrossApplication;
 import com.foreach.across.modules.adminweb.AdminWebModule;
-import com.foreach.across.modules.applicationinfo.ApplicationInfoModule;
-import com.foreach.across.modules.debugweb.DebugWebModule;
-import com.foreach.across.modules.entity.EntityModule;
 import com.foreach.across.modules.filemanager.FileManagerModule;
 import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
 import com.foreach.across.modules.properties.PropertiesModule;
-import com.foreach.across.modules.user.UserModule;
 import com.foreach.across.modules.web.AcrossWebModule;
-import com.foreach.common.spring.context.ApplicationInfo;
 import com.foreach.imageserver.admin.ImageServerAdminWebModule;
 import com.foreach.imageserver.core.ImageServerCoreModule;
 import org.springframework.boot.SpringApplication;
@@ -21,14 +16,10 @@ import java.util.Collections;
 @AcrossApplication(
 		modules = {
 				AcrossWebModule.NAME,
-				ApplicationInfoModule.NAME,
 				AdminWebModule.NAME,
-				DebugWebModule.NAME,
 				AcrossHibernateJpaModule.NAME,
-				EntityModule.NAME,
 				PropertiesModule.NAME,
-				FileManagerModule.NAME,
-				UserModule.NAME
+				FileManagerModule.NAME
 		}
 )
 public class ImageServerTestEmbeddedApplication
@@ -45,7 +36,7 @@ public class ImageServerTestEmbeddedApplication
 	}
 
 	@Bean
-	public ImageServerAdminWebModule imageServerAdminWebModule(){
+	public ImageServerAdminWebModule imageServerAdminWebModule() {
 		return new ImageServerAdminWebModule();
 	}
 }
