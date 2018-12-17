6e42f1f819ee23959e2287a45b8ba921879194b2
Steven Gentens <stg@foreach.be>
2018-12-17 10:28:12 +0100

    fix tests

--- imageserver-core/src/main/java/com/foreach/imageserver/core/config/conditional/ImageMagickTransformerConfiguration.java
+++ imageserver-core/src/main/java/com/foreach/imageserver/core/config/conditional/ImageMagickTransformerConfiguration.java
@@ -2,7 +2,6 @@ package com.foreach.imageserver.core.config.conditional;
 
 import com.foreach.imageserver.core.ImageServerCoreModuleSettings;
 import com.foreach.imageserver.core.transformers.ImageMagickImageTransformer;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 
@@ -10,13 +9,10 @@ import org.springframework.context.annotation.Bean;
  * @author Arne Vandamme
  */
 @ConditionalOnExpression("${" + ImageServerCoreModuleSettings.IMAGEMAGICK_ENABLED + ":false}")
-@RequiredArgsConstructor
 public class ImageMagickTransformerConfiguration
 {
-	private final ImageServerCoreModuleSettings settings;
-
 	@Bean
-	public ImageMagickImageTransformer imageMagickImageTransformer() {
+	public ImageMagickImageTransformer imageMagickImageTransformer( ImageServerCoreModuleSettings settings ) {
 		return new ImageMagickImageTransformer(
 				settings.getTransformers().getImageMagick().getPriority(),
 				settings.getTransformers().getImageMagick().getPath(),
